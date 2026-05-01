f0ea34e834465d80b8f5c7f06ae31bf5000a3ddf
DivyanshDixit01 <166687753+DivyanshDixit01@users.noreply.github.com>
2026-05-01 21:44:58 +0530

    Update authApi.js

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -2,7 +2,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn } from "../authSlice";
 
-const USER_API = "http://localhost:5000/api/users";
+const USER_API = "https://lms-project-5yly.onrender.com/api/users";
 
 export const authApi = createApi({
   reducerPath: "authApi",
